1bbacb53c01c287077fa9180e9a78a17c7368a18
Jonathan Adithya <jo.adithya391@gmail.com>
2021-10-22 16:24:34 -0700

    restructure files into app folder

--- application.py
+++ application.py
@@ -1,15 +1,4 @@
-from flask import Flask, render_template
-
-# Configure application
-app = Flask(__name__)
-
-
-# Routes
-@app.route('/')
-def index():
-    return render_template('index.html')
-
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-
