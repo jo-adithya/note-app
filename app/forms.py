05850c50ab51d452b8ae088dfaadf1242b912022
inferness <axelcornelius301@gmail.com>
2021-12-07 22:47:09 +0700

    added forget and register and sidebar

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+    submitReg = SubmitField('Register')
+    forgot = SubmitField('Continue')
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,21 @@ from flask import render_template
 from app import app
 from .forms import LoginForm
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
-
 
 @app.route('/login')
 def login():
     form = LoginForm()
     return render_template('login.html', form=form)
+
+@app.route('/register')
+def register():
+    form = LoginForm()
+    return render_template('register.html', form=form)
+
+@app.route('/')
+def forgot():
+    form = LoginForm()
+    return render_template('forgot.html', form=form)
