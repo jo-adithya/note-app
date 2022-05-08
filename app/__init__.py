2bde5d2ada7cbad921c749c32d05437d091011f1
Jonathan Adithya <jo.adithya391@gmail.com>
2022-05-07 17:30:40 -0700

    Add a login manager to the app

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,19 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+# Initialize Configurations
 app.config.from_object(Config)
+
+# Initialize Database Connection
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Initialize Login Manager
+login = LoginManager(app)
+login.login_view = 'login'
 
 from . import routes, models
